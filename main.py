42b388282f70306c99822dee1fa19270371d5899
casualdoto <khrestyanovskii@gmail.com>
2024-05-17 03:56:39 +0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import RaidFunctions
-address_write = input('введите адрес, в который вы хотите записать 16-ричное сообщение размером 14 байт (0-63): ')
-num = input('введите 16-ричное сообщение размером 14 байт: ')
-RaidFunctions.write(address_write, num)
-address_read = input('введите адрес, из которого вы хотите прочитать 16-ричное сообщение размером 14 байт: ')
-print(RaidFunctions.read(address_read))
+while(True):
+    message = input('если вы хотите записать число - введите 1, если прочитать - 2: ')
+    if message == '1':
+        address_write = input('введите адрес, в который вы хотите записать 16-ричное сообщение размером 14 байт (0-63): ')
+        num = input('введите 16-ричное сообщение размером 14 байт: ')
+        RaidFunctions.write(address_write, num)
+    elif message == '2':
+        address_read = input('введите адрес, из которого вы хотите прочитать 16-ричное сообщение размером 14 байт: ')
+        print(RaidFunctions.read(address_read))
